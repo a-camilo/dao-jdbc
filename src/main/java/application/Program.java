10dd52f932cb5fb24487fa04c69b9d74599cb0ce
a-camilo <acamilo10@hotmail.com>
2022-06-07 13:48:48 -0300

    Update implementation

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -1,10 +1,13 @@
 package application;
 
+import com.sun.source.tree.NewArrayTree;
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 public class Program {
@@ -30,5 +33,17 @@ public class Program {
         for (Seller obj:list) {
             System.out.println(obj);
         }
+
+        System.out.println("==== TESTE 4: seller insert ====");
+        Seller newSeller = new Seller(null,"Greg", "greg@gmail.com", Calendar.getInstance().getTime(),4000.0,department);
+        sellerDao.insert(newSeller);
+        System.out.println(newSeller.getId());
+
+        System.out.println("==== TESTE 5: seller update ====");
+        seller = sellerDao.findById(1);
+        seller.setName("Martha Waine");
+        sellerDao.update(seller);
+
+        System.out.println("Update completed");
     }
 }
